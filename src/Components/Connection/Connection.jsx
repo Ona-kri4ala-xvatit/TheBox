28dc144ea9215627ed7d606bf7d735bff0513e9c
Nijat Muradov <live4ever4@gmail.com>
2024-03-05 00:51:14 +0400

    sad but true

--- src/Components/Connection/Connection.jsx
+++ src/Components/Connection/Connection.jsx
@@ -6,25 +6,25 @@ export const Connection = () => {
         <section className="connection">
             <div className="container">
                 <div className="connection-wrapper">
-                <div className="connection-offer">
-                    <h2>What can us do for you?</h2>
-                    <div className="connection-text">
-                        We are ready to work on a project of any complexity, whether it’s commercial or residential.
+                    <div className="connection-offer">
+                        <h2>What can us do for you?</h2>
+                        <div className="connection-text">
+                            We are ready to work on a project of any complexity, whether it’s commercial or residential.
+                        </div>
                     </div>
-                </div>
-                <form action="">
-                    <div className="connection-input-wrapper">
-                        <input className="connection-input" type="name" placeholder="Name*"/>
-                        <input className="connection-input" type="mail" placeholder="Email*"/>
-                        <input className="connection-input" type="text" placeholder="Reason for Contacting*"/>
-                        <input className="connection-input" type="phone" placeholder="Phone"/>
-                        <input className="connection-input message" type="message" placeholder="Message"/>
-                    </div>
-                    <div>* indicates a required field</div>
-                    <div>
-                        <button className="connection-btn">Submit</button>
-                    </div>
-                </form>
+                    <form action="">
+                        <div className="connection-input-wrapper">
+                            <input className="connection-input" type="name" placeholder="Name*" />
+                            <input className="connection-input" type="mail" placeholder="Email*" />
+                            <input className="connection-input" type="text" placeholder="Reason for Contacting*" />
+                            <input className="connection-input" type="phone" placeholder="Phone" />
+                            <input className="connection-input message" type="message" placeholder="Message" />
+                        </div>
+                            <div className="footnote">* indicates a required field</div>
+                            <div>
+                                <button className="connection-btn">Submit</button>
+                            </div>
+                    </form>
                 </div>
             </div>
         </section>
